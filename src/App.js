d455b4b3f5de73b0231e79cb9638045464fccdd1
Olga Verbetska <verbetska150@gmail.com>
2021-04-16 17:12:10 +0300

    preparing

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Component } from 'react';
+
+
+import './styles.css';
+
+
+class App extends Component {
+
+  
+  render() {
+ 
+    return (
+      <div className="container">
+        <h1 className="title">Movies</h1>
+   
+      </div>
+    );
+  }
+}
+
+export default App;
